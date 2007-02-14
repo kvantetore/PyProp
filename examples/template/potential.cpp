8b06e81ea59125ac094cda78a928993d79dd1ee9
tore.birkeland <tore.birkeland@354cffb3-6528-0410-8d42-45ce437ad3c5>
2007-02-14 11:53:13 +0000

    Added examples/template
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@18 354cffb3-6528-0410-8d42-45ce437ad3c5

---
+++ examples/template/potential.cpp
@@ -0,0 +1,40 @@
+#include <core/wavefunction.h>
+#include <core/potential/dynamicpotentialevaluator.h>
+
+template<int Rank>
+class PotentialTemplate
+{
+public:
+	//Required by DynamicPotentialEvaluator
+	cplx TimeStep;
+	double CurTime;
+
+	//Potential parameters
+	double param;
+
+	/*
+	 * Called once with the corresponding config section
+	 * from the configuration file. Do all one time set up routines
+	 * here.
+	 */
+	void ApplyConfigSection(const ConfigSection &config)
+	{
+		config.Get("param", param);
+	}
+
+	/*
+	 * Called for every grid point at every time step. 
+	 *
+	 * Some general tips for max efficiency:
+	 * - If possible, move static computations to ApplyConfigSection.
+	 * - Minimize the number of branches ("if"-statements are bad)
+	 * - Minimize the number of function calls (sin, cos, exp, are bad)
+	 * - Long statements can confuse the compiler, consider making more 
+	 *   simpler statements
+	 */
+	inline double GetPotentialValue(const blitz::TinyVector<double, Rank> &pos)
+	{
+	}
+};
+
+
