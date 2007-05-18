5decbefb2f0591551a5a0373eaa76b19d5f45efd
tore.birkeland <tore.birkeland@354cffb3-6528-0410-8d42-45ce437ad3c5>
2007-05-18 14:35:00 +0000

    Added blitzblas wrappers to pyprop.core in order to ease testing of the blas routines
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@76 354cffb3-6528-0410-8d42-45ce437ad3c5

---
+++ core/python/blitzblas_wrapper.cpp
@@ -0,0 +1,54 @@
+
+// Boost Includes ==============================================================
+#include <boost/python.hpp>
+#include <boost/cstdint.hpp>
+
+// Includes ====================================================================
+#include <utility/blitzblas.h>
+
+// Using =======================================================================
+using namespace boost::python;
+
+void MatrixMatrixMultiply_cplx(blitz::Array<cplx, 2> A, blitz::Array<cplx, 2> B, blitz::Array<cplx, 2> C)
+{
+	MatrixMatrixMultiply(A, B, C);
+}
+
+void MatrixMatrixMultiply_double(blitz::Array<double, 2> A, blitz::Array<double, 2> B, blitz::Array<double, 2> C)
+{
+	MatrixMatrixMultiply(A, B, C);
+}
+
+void MatrixVectorMultiply_cplx(blitz::Array<cplx, 2> A, blitz::Array<cplx, 1> v, blitz::Array<cplx, 1> w)
+{
+	MatrixVectorMultiply(A, v, w);
+}
+
+void MatrixVectorMultiply_double(blitz::Array<double, 2> A, blitz::Array<double, 1> v, blitz::Array<double, 1> w)
+{
+	MatrixVectorMultiply(A, v, w);
+}
+
+
+/*
+template<int Rank>
+void VectorElementMultiply(blitz::Array<cplx, Rank> u, blitz::Array<cplx, Rank> v, blitz::Array<cplx, Rank> w);
+template<int Rank>
+void VectorElementMultiply(blitz::Array<double, Rank> u, blitz::Array<double, Rank> v, blitz::Array<double, Rank> w);
+
+template<int Rank>
+double VectorInnerProduct(blitz::Array<double, Rank> u, blitz::Array<double, Rank> v);
+template<int Rank>
+cplx VectorInnerProduct(blitz::Array<cplx, Rank> u, blitz::Array<cplx, Rank> v);
+*/
+
+
+// Module ======================================================================
+void ExportBlitzBlas()
+{
+	def("MatrixMatrixMultiply", MatrixMatrixMultiply_double);
+	def("MatrixVectorMultiply", MatrixVectorMultiply_double);
+	//def("VectorElementMultiply_1", VectorElementMultiply<1>);
+	//def("VectorInnerProduct_1", VectorInnerProduct<1>);
+}
+
