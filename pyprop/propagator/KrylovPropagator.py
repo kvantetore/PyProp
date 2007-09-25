35f1b6b339c6c648adc1886e44a476a6ef624289
tore.birkeland <tore.birkeland@354cffb3-6528-0410-8d42-45ce437ad3c5>
2007-09-25 08:00:26 +0000

    Ooops3
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@99 354cffb3-6528-0410-8d42-45ce437ad3c5

---
+++ pyprop/propagator/KrylovPropagator.py
@@ -0,0 +1,58 @@
+
+class ExpokitPropagator(PropagatorBase):
+	__Base = PropagatorBase
+	
+	def __init__(self, psi):
+		self.__Base.__init__(self, psi)
+		self.Rank = psi.GetRank()
+		
+		self.ExpokitPropagator = CreateInstanceRank("core.krylov_ExpokitPropagator", self.Rank)
+
+	def ApplyConfig(self, config):
+		self.__Base.ApplyConfig(self, config)
+
+		#Set up the base propagator (which will perform matrix vector multiplications)
+		propagatorType = config.Propagation.base_propagator
+		self.BasePropagator = propagatorType(self.psi)
+		config.Apply(self.BasePropagator)
+		#Set up the expokit propagator
+		config.Apply(self.ExpokitPropagator)
+
+	def ApplyConfigSection(self, configSection): 
+		self.__Base.ApplyConfigSection(self, configSection)
+
+		#Set up base propagator
+		configSection.Apply(self.BasePropagator)
+		#Set up the expokit propagator
+		configSection.Apply(self.ExpokitPropagator)
+	
+	def SetupStep(self, dt):
+		self.BasePropagator.SetupStep(dt)
+		self.ExpokitPropagator.Setup(self.psi);
+		#We need an additional wavefunction to perform matrix-vector muls
+		self.TempPsi = self.psi.CopyDeep()
+
+	def MultiplyHamiltonian(self, destPsi, t, dt):
+		self.BasePropagator.MultiplyHamiltonian(destPsi, t, dt)
+
+	def AdvanceStep(self, t, dt):
+		self.ExpokitPropagator.AdvanceStep(self.MatVecCallback, self.psi, self.TempPsi, dt, t)
+
+	def MatVecCallback(self, psi, tempPsi, dt, t):
+		#assert psi == self.psi
+		#assert tempPsi == self.TempPsi
+		
+		#inN = psi.GetNorm()
+
+		self.BasePropagator.MultiplyHamiltonian(tempPsi, t, dt)
+
+		#outN = self.TempPsi.GetNorm()
+		#if outN < 0.001:
+		#	pylab.plot(abs(psi.GetData())**2)
+		#	pylab.plot(abs(tempPsi.GetData())**2)
+		#	print "inN = ", inN
+		#	print "outN = ", outN
+		#	assert False
+	
+
+
---
+++ pyprop/propagator/subpropagator/SphericalPropagatorBase.py
@@ -0,0 +1,45 @@
+
+class SphericalPropagatorBase:
+	def __init__(self, psi, transformRank):
+		self.psi = psi
+		self.TransformRank = transformRank
+
+		if transformRank != psi.GetRank() - 1:
+			raise "SphericalTransform can only be used on the last rank"
+
+	def ApplyConfigSection(self, config):
+		self.Mass = config.mass
+		self.RadialRank = config.radial_rank
+
+	def SetupStep(self, dt):
+		raise "Implement this in a inheriting class"
+
+	def SetupStepConjugate(self, dt):
+		#Transform from Grid to Spherical Harmonics
+		self.Transform.ForwardTransform(self.psi)
+		self.psi.GetRepresentation().SetRepresentation(self.TransformRank, self.RepresentationSphericalHarmonic)
+
+	def AdvanceStep(self, t, dt):
+		self.Potential.AdvanceStep(t, dt)
+		self.Transform.InverseTransform(self.psi)
+		self.psi.GetRepresentation().SetRepresentation(self.TransformRank, self.RepresentationTheta)
+
+	def AdvanceStepConjugate(self, t, dt):
+		self.Transform.ForwardTransform(self.psi)
+		self.Potential.AdvanceStep(t, dt)
+		self.psi.GetRepresentation().SetRepresentation(self.TransformRank, self.RepresentationSphericalHarmonic)
+
+	def MultiplyHamiltonian(self, dstPsi, t, dt):
+		self.Potential.MultiplyPotential(dstPsi, t, dt)
+		self.Transform.InverseTransform(self.psi)
+		self.Transform.InverseTransform(dstPsi)
+		self.psi.GetRepresentation().SetRepresentation(self.TransformRank, self.RepresentationTheta)
+		dstPsi.GetRepresentation().SetRepresentation(self.TransformRank, self.RepresentationTheta)
+
+	def MultiplyHamiltonianConjugate(self, dstPsi, t, dt):
+		self.Transform.ForwardTransform(self.psi)
+		self.Transform.ForwardTransform(dstPsi)
+		self.psi.GetRepresentation().SetRepresentation(self.TransformRank, self.RepresentationSphericalHarmonic)
+		dstPsi.GetRepresentation().SetRepresentation(self.TransformRank, self.RepresentationSphericalHarmonic)
+
+
