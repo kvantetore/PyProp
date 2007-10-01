fb5947f8fb25dc9d27dd8e977e803f61bf2e18ba
tore.birkeland <tore.birkeland@354cffb3-6528-0410-8d42-45ce437ad3c5>
2007-10-01 12:54:30 +0000

    - Added CartesianBoundaryAbsorbingPotential after spec. from Lene
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@101 354cffb3-6528-0410-8d42-45ce437ad3c5

--- core/krylov/expokitpropagator.cpp
+++ core/krylov/expokitpropagator.cpp
@@ -3,75 +3,12 @@
 
 #include <core/wavefunction.h>
 
+#include "krylovcommon.h"
+
 namespace krylov
 {
 
 using namespace boost::python;
-
-
-template<int Rank>
-void MultiplyHamiltonian(void *data, cplx *inBuffer, cplx *outBuffer)
-{
-	typedef blitz::TinyVector<int, Rank> DataVector;
-	typedef blitz::Array<cplx, Rank> DataArray;
-
-	if (inBuffer == outBuffer)
-	{
-		cout << "Error: both input and output-buffer is the same." << endl;
-		cout << "  iBuffer address = " << inBuffer << endl;
-		cout << "  oBuffer address = " << outBuffer << endl;
-	}
-
-	//Some local variables for simplicity
-	ExpokitPropagator<Rank> *propagator = static_cast<ExpokitPropagator<Rank>*>(data);
-	Wavefunction<Rank> *psi = propagator->Psi;
-	Wavefunction<Rank> *tempPsi = propagator->TempPsi;
-
-	//Wrap the data buffers in blitz arrays and initialize outdata to 0
-	DataVector shape = psi->GetData().shape();
-	DataVector stride = psi->GetData().stride();
-	DataArray inData(inBuffer, shape, stride, blitz::neverDeleteData);
-	DataArray outData(outBuffer, shape, stride, blitz::neverDeleteData);
-	outData = 0;
-
-	cplx inNorm = sum((inData * conj(inData)));
-	//cout << "In Norm = " << inNorm << endl;
-
-	//Remember the original data buffers of psi and tempPsi
-	DataArray psiOrigData(psi->GetData());
-	DataArray tempPsiOrigData(tempPsi->GetData());
-	//Use the data buffers supplied by expokit
-	psi->SetData(inData);
-	tempPsi->SetData(outData);
-
-	//Perform Hamilton-Wavefunction multiplication
-	propagator->MultiplyCallback(psi, tempPsi, propagator->TimeStep, propagator->CurTime);
-
-	//Restore the databuffers of psi and tempPsi
-	psi->SetData(psiOrigData);
-	tempPsi->SetData(tempPsiOrigData);
-
-	//Scale with negative imaginary unit if we do NOT use imaginary time.
-	if (!propagator->ImaginaryTime)
-	{
-		outData *= cplx(0.0, -1.0);
-	}
-	else
-	{
-		outData *= -1.0;
-	}
-
-	cplx outNorm = sum((outData * conj(outData)));
-	if (real(outNorm) < 0.1)
-	{
-		cout << "Outnorm is very small. Something is amiss." << endl;
-		cout << "  InNorm = " << inNorm << endl;
-		cout << " outNorm = " << outNorm << endl;
-		cout << "  inAddr = " << inBuffer << endl;
-		cout << " outAddr = " << outBuffer << endl;
-		cout << "    diff = " << (int)(inBuffer - outBuffer) << endl;
-	}
-}
 
 
 template<int Rank>
--- core/potential/cartesianabsorber.h
+++ core/potential/cartesianabsorber.h
@@ -55,5 +55,74 @@ public:
 };
 
 
+template<int Rank>
+class CartesianBoundaryAbsorbingPotential
+{
+public:
+	typedef blitz::TinyVector< blitz::Array<double, 1>, Rank> GridVector;
+	typedef blitz::TinyVector<double, Rank> DoubleVector;
+	typedef blitz::Array<cplx, Rank> DataArray;
+	typedef blitz::Array<double, 1> GridArray;
+
+	double AbsorbtionWidth;
+
+	void ApplyConfigSection(const ConfigSection &config)
+	{
+		config.Get("absorbtion_width", AbsorbtionWidth);
+	}
+
+	/** Updates propagates the wavefunction a step with this dynamic potential **/
+	void ApplyPotential(Wavefunction<Rank> &psi, cplx timeStep, double curTime)
+	{
+		//Set up grid
+		GridVector grid;
+		Representation<Rank> *reprBasic = &psi.GetRepresentation();
+		CartesianRepresentation<Rank> *repr = (CartesianRepresentation<Rank>*) reprBasic;
+		for (int curRank = 0; curRank<Rank; curRank++)
+		{
+			grid(curRank).reference(repr->GetLocalGrid(curRank));
+		}
+
+		//Max values
+		DoubleVector maxPos;
+		DoubleVector minPos;
+		for (int curRank = 0; curRank<Rank; curRank++)
+		{
+			maxPos(curRank) = repr->GetRange(curRank).Max;
+			minPos(curRank) = repr->GetRange(curRank).Min;
+		}
+		
+		//Iterate
+		blitz::TinyVector<double, Rank> pos;
+		blitz::TinyVector<int, Rank> indexPos;
+		typename blitz::Array<cplx, Rank>::iterator it = psi.Data.begin();
+		for (int linearCount=0; linearCount<psi.Data.size(); linearCount++)
+		{
+			for (int curRank=0; curRank<Rank; curRank++)
+			{
+				pos(curRank) = grid(curRank)(it.position()(curRank));
+			}
+
+			double scale = 1;
+			for (int curRank=0; curRank<Rank; curRank++)
+			{ 
+				if (pos(curRank) < minPos(curRank) + AbsorbtionWidth)
+				{
+					scale *= pow(cos(M_PI/2.0 * (pos(curRank) - (minPos(curRank) + AbsorbtionWidth)) / AbsorbtionWidth), 1/8.0);
+				}
+				if (pos(curRank) > maxPos(curRank) - AbsorbtionWidth)
+				{
+					scale *= pow(cos(M_PI/2.0 * (- maxPos(curRank) + pos(curRank) + AbsorbtionWidth) / AbsorbtionWidth), 1/8.0);
+				}
+			}
+			*it *= scale;
+			
+			it++;
+		}
+	}
+
+};
+
+
 #endif
 
