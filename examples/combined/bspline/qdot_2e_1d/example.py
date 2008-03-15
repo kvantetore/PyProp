9091b179a6e714e096f6b7d618d937879a476870
raymond.nepstad <raymond.nepstad@354cffb3-6528-0410-8d42-45ce437ad3c5>
2008-03-15 14:28:21 +0000

    -Added a 2-electron 1D qdot example
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@228 354cffb3-6528-0410-8d42-45ce437ad3c5

---
+++ examples/combined/bspline/qdot_2e_1d/example.py
@@ -0,0 +1,151 @@
+#Import system modules
+import sys
+import os
+from pylab import *
+from numpy import *
+
+#Load pyprop
+sys.path.insert(1, os.path.abspath("./pyprop"))
+import pyprop
+pyprop = reload(pyprop)
+
+#Load the project module
+from libpotential import *
+
+def SetupConfig(**args):
+	#Decide which config file to use
+	configFile = "config.ini"
+	if "config" in args:
+		configFile = args["config"]
+
+	#Load the config file
+	conf = pyprop.Load(configFile)
+
+	#Modify the config
+	if "imtime" in args:
+		imtime = args["imtime"]
+		propSection = conf.Propagation
+		dt = abs(propSection.timestep)
+		renormalize = False
+		if imtime:
+			dt = -1.0j * dt
+			renormalize = True
+
+		propSection.timestep = dt
+		propSection.renormalization = renormalize
+
+	if "amplitude" in args:
+		amplitude = args["amplitude"]
+		conf.DynamicPotential.amplitude = amplitude
+
+	
+	if "xmax" in args:
+		xMax = args['xmax']
+		conf.BSplineRepresentation.xmax = xMax
+	
+	if "xsize" in args:
+		xSize = args['xsize']
+		conf.BSplineRepresentation.xsize = xSize
+	
+	if "dt" in args:
+		timeStep = args['dt']
+		conf.Propagation.timestep = timeStep
+		
+	return conf
+
+
+def SetupProblem(**args):
+	conf = SetupConfig(**args)
+	prop = pyprop.Problem(conf)
+	prop.SetupStep()
+	return prop
+
+
+def FindGroundstate(**args):
+	args['imtime'] = True
+	prop = SetupProblem(**args)
+	
+	for t in prop.Advance(10):
+		E = prop.GetEnergy()
+		print "t = %f, E = %f" % (t, E)
+
+	E = prop.GetEnergy()
+	print "Ground State Energy = %f" % E
+
+	SaveWavefunction("groundstate.h5", "/wavefunction", prop.psi)
+
+	return prop
+
+
+def FindEigenvalues(**args):
+	prop = SetupProblem(**args)
+	solver = pyprop.PiramSolver(prop)
+	solver.Solve()
+	
+	for E in solver.GetEigenvalues():
+		print "%.17f" % E
+
+	return solver
+
+
+def SaveWavefunction(filename, dataset, psi):
+	if pyprop.ProcId == 0:
+		if os.path.exists(filename):
+			os.unlink(filename)
+	pyprop.serialization.SaveWavefunctionHDF(filename, dataset, psi)
+
+
+def GetHamiltonMatrixSubspace(prop, I, J):
+	size = prop.psi.GetData().shape[0]
+	matrix = zeros((size, size), dtype=complex)
+	tempPsi = prop.GetTempPsi()
+
+	for i in range(size):
+		prop.psi.GetData()[:] = 0
+		prop.psi.GetData()[i,I] = 1
+
+		tempPsi.GetData()[:] = 0
+		prop.MultiplyHamiltonian(tempPsi)
+		
+		matrix[:, i] = tempPsi.GetData()[:,J]
+		
+	return matrix
+
+
+def GetHamiltonMatrix(prop):
+	BSplineObject = prop.psi.GetRepresentation().GetRepresentation(0).GetBSplineObject()
+	k = BSplineObject.MaxSplineOrder
+
+	size = prop.psi.GetData().size
+	wfSize = prop.psi.GetData().shape[0]
+	tempPsi = prop.GetTempPsi()
+	matrix = zeros((size, size))
+	matrix[:,:] = 0
+
+	for i in range(wfSize):
+		print "%i of %i" % (i, wfSize)
+		for j in range(wfSize):
+			prop.psi.GetData()[:] = 0
+			prop.psi.GetData()[i,j] = 1
+
+			tempPsi.GetData()[:] = 0
+			prop.MultiplyHamiltonian(tempPsi)
+			
+			startI = j * wfSize
+			stopI = startI + wfSize
+			for k in range(wfSize):
+				J = k * wfSize + i
+				matrix[startI:stopI, J] = tempPsi.GetData()[:,k]
+
+	return matrix
+
+
+def FindEigenvalues(**args):
+	prop = SetupProblem(**args)
+	solver = pyprop.PiramSolver(prop)
+	solver.Solve()
+	
+	for E in solver.GetEigenvalues():
+		print "%.17f" % E
+
+	return solver
