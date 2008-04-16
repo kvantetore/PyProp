30b42f58cdd572c7f51bce15a9259a1f95dead8d
tore.birkeland <tore.birkeland@354cffb3-6528-0410-8d42-45ce437ad3c5>
2008-04-16 17:16:02 +0000

    Added submitpbs_stallo
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@319 354cffb3-6528-0410-8d42-45ce437ad3c5

---
+++ pyprop/utilities/submitpbs_stallo.py
@@ -0,0 +1,111 @@
+import os
+import sys
+import commands
+
+from datetime import timedelta
+
+"""
+Utilities for submitting jobs to the PBS scheduling system used
+on stallo.uit.no.
+"""
+
+
+class SubmitScript:
+	#resources
+	walltime = timedelta(hours=0, minutes=30, seconds=0)
+	nodes = 1
+	ppn = 1
+	proc_memory = None 
+	
+	account = None 
+	jobname = "myjob"
+
+	stdout = None
+	stdin = None
+	stderr = None
+
+	executable = "a.out"
+	parameters = ""
+	workingdir = None
+
+	interconnect = "ib" # "ib" or None
+	
+
+	def CreateScript(self):
+		script = list()
+
+		#preamble
+		script.append("#! /bin/bash -")
+		script.append("#PBS -S /bin/bash")
+
+		#Resources
+		#time
+		hours = self.walltime.days * 24 + (self.walltime.seconds / 3600)
+		minutes = (self.walltime.seconds / 60) % 60
+		seconds = self.walltime.seconds % 60
+		script.append("#PBS -l walltime=%i:%i:%i" % (hours, minutes, seconds))
+
+		#procs
+		if self.interconnect == "ib":
+			interconnect = ":ib"
+		else:
+			interconnect = ""
+		script.append("#PBS -l nodes=%i:ppn=%i%s" % (self.nodes, self.ppn, interconnect))
+
+		#mem
+		if self.proc_memory != None:
+			script.append("#PBS -l pvmem=" + str(self.proc_memory))
+
+		#Administrative
+		script.append('#PBS -N "%s"' % (self.jobname,))
+		if self.account != None:
+			script.append("#PBS -A %s" % (self.account,))
+
+		#IO redirection
+		if self.stdout != None:
+			script.append('#PBS -o "%s"' % (self.stdout))
+		if self.stderr != None:
+			script.append('#PBS -e "%s"' % (self.stderr))
+
+		#Working dir
+		if self.workingdir == None:
+			self.workingdir = os.path.abspath(os.curdir)
+		script.append("cd %s" % (self.workingdir))
+
+		#Check if we're redirecting stdin
+		instr = ""
+		if self.stdin != None:
+			instr = "< " + str(self.stdin)
+	
+		#Create script line
+		script.append(str(self.executable) + " " + str(self.parameters) + instr)
+		
+		#exit stuff
+		script.append("exit $?")
+
+		return script
+
+
+	def WriteScript(self, filename):
+		script = self.CreateScript()
+		f = file(filename, 'w')
+		for line in script:
+			f.write(line)
+			f.write("\n")
+		f.close()
+		
+	def Submit(self):
+		#create a temporary file for the script
+		tempName = os.tempnam(".", "scrpt")
+		self.WriteScript(tempName)
+
+		#submit script
+		jobName = commands.getoutput("qsub " + tempName)
+
+		#delete temporary script file
+		os.remove(tempName)
+
+		print jobName + " submitted"
+
+		return jobName
+	
