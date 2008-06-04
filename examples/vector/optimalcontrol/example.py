1a8029668d383ea6f16ce8d0ffce005ad4e51fc8
raymond.nepstad <raymond.nepstad@354cffb3-6528-0410-8d42-45ce437ad3c5>
2008-06-04 11:59:07 +0000

    -Cleaned up Optimal Control example
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@370 354cffb3-6528-0410-8d42-45ce437ad3c5

--- examples/vector/optimalcontrol/example.py
+++ examples/vector/optimalcontrol/example.py
@@ -13,31 +13,6 @@ sys.path.append("pyprop")
 import pyprop; 
 pyprop = reload(pyprop)
 
-def GetMatrixQdot4(psi, conf):
-	"""
-	Get couplings between the four states considered for
-	the optimal control PRL (Saelen et al 2008).
-	"""
-	scaling = conf.scaling
-	V12 = -1.0067555306 * scaling
-	V13 = 0.0
-	V14 = -0.069677839373 * scaling
-	V23 = -1.0501555811  * scaling 
-	V24 = 0.0
-	V34 = -3.0758641281 * scaling
-	potential = array([ [0, V12, V13, V14], \
-	                    [V12, 0, V23, V24], \
-	                    [V13, V23, 0, V34], \
-	                    [V14, V24, V34, 0]],\
-	                    dtype=complex)
-	
-	return potential
-
-def GetDiagonalElementsQdot4(psi, config, potential):
-	"""
-	"""
-	#potential[:] = [0.241995, 0.324479, 0.380237, 0.382176]
-	potential[:] = [-0.71337255649561E-01, 0.11146318930044E-01, 0.66904900928925E-01, 0.68844014109733E-01]
 
 def GetDiagonalElements(psi, config, potential):
 	"""
@@ -45,17 +20,18 @@ def GetDiagonalElements(psi, config, potential):
 	"""
 	h5file = tables.openFile(config.filename, "r")
 	try:
-		potential[:] = h5file.getNode(config.dataset)[:]
+		potential[:] = h5file.getNode(config.dataset)[:config.size]
 		potential[:] *= config.scaling	
 	finally:
 		h5file.close()
-	#potential[:] = pylab.load(config.file_name)[:config.size] * config.scaling
 
 
 def Setup(**args):
 	"""
 	Setup optimal control problem
 	"""
+	print "Setting up control problem"
+
 	configFile = 'config.ini'
 	if "config" in args:
 		configFile = args["config"]
@@ -66,11 +42,9 @@ def Setup(**args):
 		conf.Propagation.timestep = args["dt"]
 
 	controlAlgorithm = args.get("controlAlgorithm", "Krotov")
+	print "    Using control algorithm: %s" % controlAlgorithm
 	confSection = eval("conf.%s" % controlAlgorithm)
 
-	if args.get("perturbControl", False):
-		confSection.perturb_control = 1e-15
-	
 	if "bwdUpdate" in args:
 		confSection.update_backwards = args["bwdUpdate"]
 		print "Backward update: %s" % args["bwdUpdate"]
@@ -87,35 +61,6 @@ def Setup(**args):
 	return controlSolver
 
 
-def SetupZhuRabitz(**args):
-	"""
-	Setup ZhuRabitz problem
-	"""
-	conf = pyprop.Load('config.ini')
-
-	if "timestep" in args:
-		config.Propagation.timestep = args["timestep"]
-
-	prop = pyprop.Problem(conf)
-	prop.SetupStep()
-	zhurabitz = pyprop.ZhuRabitz(prop)
-	return zhurabitz
-
-def SetupDegani(**args):
-	"""
-	Setup Degani problem
-	"""
-	conf = pyprop.Load('config.ini')
-
-	if "timestep" in args:
-		config.Propagation.timestep = args["timestep"]
-
-
-	prop = pyprop.Problem(conf)
-	prop.SetupStep()
-	degani = pyprop.Degani(prop)
-	return degani
-
 def Run():
 	krotov = Setup()
 	krotov.Run()
@@ -123,35 +68,11 @@ def Run():
 	return krotov
 
 def GetPulseSpectrum(krotov, whichControl):
-	"""
-	"""
-	
-	dw = 2 * pi * fft.fftshift(fft.fftfreq(len(krotov.ControlVectors[whichControl]), krotov.TimeGridResolution))
+	dw = 2 * pi * fft.fftshift(fft.fftfreq(len(krotov.ControlVectors[whichControl]), \
+		krotov.TimeGridResolution))
 	pulseSpectrum = fft.fftshift(fft.fft(krotov.ControlVectors[whichControl]))
-
 	return dw, pulseSpectrum
 
-
-def Commutator(A,B):
-	"""
-	Computes the commutator [A,B] = AB-BA
-	"""
-	return dot(A,B) - dot(B, A)
-
-
-def NestedCommutatorTypeI(A, B, depth):
-	"""
-	Computes the nested commutator [A,[A,...,[A,B]...]],
-	that is, an inner commutator of C = [A,B] and then
-	then recursively the commutator [A,C] until desired 
-	depth is reached.
-	"""
-	C = Commutator(A,B)
-	for d in range(depth):
-		C = Commutator(A,C)
-	
-	return C
-	
 
 def TextToHDFDense(fileName, vectorSize, scaling):
 
@@ -175,78 +96,3 @@ def TextToHDFDense(fileName, vectorSize, scaling):
 
 	finally:
 		fileh5.close()
-
-def RunStabilityExperiment(**args):
-	args["perturbControl"] = True
-		
-	controlProblem = Setup(**args)
-
-	controlProblem.Run()
-	v1 = controlProblem.ControlVectors[0,:].copy()
-
-	controlProblem = Setup(**args)
-	controlProblem.Run()
-	v2 = controlProblem.ControlVectors[0,:].copy()
-
-	semilogy(linspace(0, controlProblem.PropagationTime, controlProblem.TimeGridSize), abs(v1 - v2))
-
-	return v1, v2
-
-def RunExperiment1():
-	"""
-
-	"""
-	
-	workDir = "experiments/number_1"
-
-	#Load config and setup problem
-	conf = pyprop.Load("%s/config.ini" % workDir)
-	prop = pyprop.Problem(conf)
-	prop.SetupStep()
-	
-	h5file = tables.openFile("%s/results.h5" % workDir, "w")
-	try:
-		#Run Krotov
-		krotov = pyprop.Krotov(prop)
-		krotov.Run()
-		h5file.createGroup("/","krotov")
-		h5file.createArray("/krotov", "J", krotov.J)
-		h5file.createArray("/krotov", "Yield", krotov.Yield)
-		h5file.createArray("/krotov", "Control", krotov.ControlVectors)
-	
-		#Run Krotov w/backward update
-		prop = pyprop.Problem(conf)
-		prop.SetupStep()
-		prop.Config.Krotov.update_backwards = True
-		krotovb = pyprop.Krotov(prop)
-		krotovb.Run()
-		h5file.createArray("/krotov_b", "J", krotovb.J)
-		h5file.createArray("/krotov_b", "Yield", krotovb.Yield)
-		h5file.createArray("/krotov_b", "Control", krotovb.ControlVectors)
-
-		#Run Zhu-Rabitz
-		prop = pyprop.Problem(conf)
-		prop.SetupStep()
-		zhurabitz = pyprop.ZhuRabitz(prop)
-		zhurabitz.Run()
-		h5file.createGroup("/","zhurabitz")
-		h5file.createArray("/zhurabitz", "J", zhurabitz.J)
-		h5file.createArray("/zhurabitz", "Yield", zhurabitz.Yield)
-		h5file.createArray("/zhurabitz", "Control", zhurabitz.ControlVectors)
-
-		#Run Degani
-		prop = pyprop.Problem(conf)
-		prop.SetupStep()
-		degani = pyprop.Degani(prop)
-		degani.Run()
-		h5file.createGroup("/","degani")
-		h5file.createArray("/degani", "J", degani.J)
-		h5file.createArray("/degani", "Yield", degani.Yield)
-		h5file.createArray("/degani", "Control", degani.ControlVectors)
-
-		#Store config file
-		h5file.createGround("/", "config")
-		h5file.setNodeAttr("/config", "configObject", conf.cfgObj)
-
-	finally:
-		h5file.close()
