a2997ac2936a9771f74144158e430d8afb452719
raymond.nepstad <raymond.nepstad@354cffb3-6528-0410-8d42-45ce437ad3c5>
2008-07-07 16:40:29 +0000

    added hydrogen-ionization b-spline example
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@407 354cffb3-6528-0410-8d42-45ce437ad3c5

---
+++ examples/combined/bspline/hydrogen_ionization/example.py
@@ -0,0 +1,132 @@
+#Import system modules
+import sys
+import os
+from pylab import *
+from numpy import *
+
+#Load pyprop
+sys.path.insert(1, os.path.abspath("./pyprop"))
+import pyprop
+pyprop = reload(pyprop)
+
+#Load the project module
+from libpotential import *
+
+def SetupConfig(**args):
+	#Decide which config file to use
+	configFile = "config.ini"
+	if "config" in args:
+		configFile = args["config"]
+
+	#Load the config file
+	conf = pyprop.Load(configFile)
+
+	#Modify the config
+	if "imtime" in args:
+		imtime = args["imtime"]
+		propSection = conf.Propagation
+		dt = abs(propSection.timestep)
+		renormalize = False
+		if imtime:
+			dt = -1.0j * dt
+			renormalize = True
+
+		propSection.timestep = dt
+		propSection.renormalization = renormalize
+
+	if "amplitude" in args:
+		amplitude = args["amplitude"]
+		conf.DynamicPotential.amplitude = amplitude
+
+	if "duration" in args:
+		duration = args["duration"]
+		conf.Propagation.duration = duration
+		
+	return conf
+
+def SetupProblem(**args):
+	conf = SetupConfig(**args)
+	prop = pyprop.Problem(conf)
+	prop.SetupStep()
+	return prop
+
+def FindGroundstate(**args):
+	args['imtime'] = True
+	prop = SetupProblem(**args)
+	
+	for t in prop.Advance(10):
+		E = prop.GetEnergy()
+		print "t = %f, E = %f" % (t, E)
+
+	E = prop.GetEnergy()
+	print "Ground State Energy = %f" % E
+
+	prop.SaveWavefunctionHDF("groundstate.h5", "/wavefunction")
+
+	return prop
+
+#def SaveWavefunction(filename, dataset, psi):
+#	if pyprop.ProcId == 0:
+#		if os.path.exists(filename):
+#			os.unlink(filename)
+#	pyprop.serialization.SaveWavefunctionHDF(filename, dataset, psi)
+
+def FindIonizationProbability(**args):
+	args['imtime'] = False
+	args['config'] = "propagation.ini"
+	prop = SetupProblem(**args)
+
+	initialPsi = prop.psi.Copy()
+
+	timeList = []
+	corrList = []
+	hold(False)
+	for t in prop.Advance(30):
+		norm = prop.psi.GetNorm()
+		corr = abs(prop.psi.InnerProduct(initialPsi))**2
+		timeList.append(t)
+		corrList.append(corr)
+		print "t = %f, Norm = %f, Corr = %f" % (t, norm, corr)
+		pcolormesh(abs(prop.psi.GetData()))
+
+	norm = prop.psi.GetNorm()
+	corr = abs(prop.psi.InnerProduct(initialPsi))**2
+	print "Final norm = %f" % norm
+	print "Initial state correlation = %f" % corr
+
+	prop.TimeList = array(timeList)
+	prop.CorrelationList = array(corrList)
+
+	return prop
+
+def FindIonizationProbabilityAmplitude():
+	amplitudeList = r_[0:2:0.2]
+	ionizationList = zeros(len(amplitudeList), dtype=double)
+
+	for i in range(len(amplitudeList)):
+		prop = FindIonizationProbability(amplitude=amplitudeList[i])
+		ionizationList[i] = 1.0 - prop.psi.GetNorm()
+
+	plot(amplitudeList, ionizationList, label="Ionization Probability")
+	xlabel("Electric Field Strength")
+	ylabel("Ionization Probability")
+	legend()
+
+	return amplitudeList, ionizationList
+
+
+def GetHamiltonMatrixSubspace(prop, l):
+	size = prop.psi.GetData().shape[0]
+	matrix = zeros((size, size), dtype=complex)
+	tempPsi = prop.GetTempPsi()
+
+	for i in range(size):
+		prop.psi.GetData()[:] = 0
+		prop.psi.GetData()[i,l] = 1
+
+		tempPsi.GetData()[:] = 0
+		prop.MultiplyHamiltonian(tempPsi)
+		
+		matrix[:, i] = tempPsi.GetData()[:,l]
+		
+	return matrix
---
+++ examples/combined/bspline/hydrogen_ionization/potential.cpp
@@ -0,0 +1,245 @@
+#include <core/wavefunction.h>
+#include <core/potential/dynamicpotentialevaluator.h>
+#include <core/potential/rankonepotentialevaluator.h>
+
+template<int Rank>
+class LaserPotential : public PotentialBase<Rank>
+{
+public:
+	//Required by DynamicPotentialEvaluator
+	cplx TimeStep;
+	double CurTime;
+
+	//Potential parameters
+	double PulseDuration;
+	double Frequency;
+	double Amplitude;
+
+	//Calculated parameters
+	double convolutionFrequency;
+	double currentAmplitude;
+
+	/*
+	 * Called once with the corresponding config section
+	 * from the configuration file. Do all one time set up routines
+	 * here.
+	 */
+	void ApplyConfigSection(const ConfigSection &config)
+	{
+		config.Get("pulse_duration", PulseDuration);
+		config.Get("frequency", Frequency);
+		config.Get("amplitude", Amplitude);
+
+		convolutionFrequency = M_PI / PulseDuration;
+	}
+
+	/*
+	 * Called once every timestep
+	 */
+	void CurTimeUpdated()
+	{
+		if (CurTime > PulseDuration)
+		{
+			currentAmplitude = 0;
+		}
+		else
+		{
+			currentAmplitude = Amplitude;
+			currentAmplitude *= sqr(sin(CurTime * convolutionFrequency));
+			currentAmplitude *= cos(CurTime * Frequency);
+		}
+	}
+
+	/*
+	 * Called for every grid point at every time step. 
+	 */
+	inline double GetPotentialValue(const blitz::TinyVector<double, Rank> &pos)
+	{
+		double r = std::abs(pos(0));
+		double theta = pos(1);
+
+		double z = r * cos(theta);
+		return currentAmplitude * z;
+	}
+};
+
+
+template<int Rank>
+class LaserPotentialFancy : public PotentialBase<Rank>
+{
+public:
+	//Required by DynamicPotentialEvaluator
+	cplx TimeStep;
+	double CurTime;
+
+	//Potential parameters
+	double PulseDuration;
+	double Frequency;
+	double Amplitude;
+
+	//Calculated parameters
+	double convolutionFrequency;
+	double currentAmplitude;
+
+	/*
+	 * Called once with the corresponding config section
+	 * from the configuration file. Do all one time set up routines
+	 * here.
+	 */
+	void ApplyConfigSection(const ConfigSection &config)
+	{
+		config.Get("pulse_duration", PulseDuration);
+		config.Get("frequency", Frequency);
+		config.Get("amplitude", Amplitude);
+
+		convolutionFrequency = M_PI / PulseDuration;
+	}
+
+	/*
+	 * Called once every timestep
+	 */
+	void CurTimeUpdated()
+	{
+		if (CurTime < PulseDuration)
+		{
+			currentAmplitude = Amplitude;
+			currentAmplitude *= sin(M_PI * CurTime / PulseDuration) 
+				* (-2 * M_PI / PulseDuration * cos(M_PI * CurTime / PulseDuration) * cos(Frequency * CurTime) 
+				+ Frequency * sin(M_PI * CurTime / PulseDuration) * sin(Frequency * CurTime));
+		}
+		else
+		{
+			currentAmplitude = 0;
+		}
+	}
+
+	/*
+	 * Called for every grid point at every time step. 
+	 */
+	inline double GetPotentialValue(const blitz::TinyVector<double, Rank> &pos)
+	{
+		double r = std::abs(pos(0));
+		double theta = pos(1);
+
+		double z = r * cos(theta);
+		return currentAmplitude * z;
+	}
+};
+
+template<int Rank>
+class StarkPotential : public PotentialBase<Rank>
+{
+public:
+	//Potential parameters
+	double FieldStrength;
+
+	/*
+	 * Called once with the corresponding config section
+	 * from the configuration file. Do all one time set up routines
+	 * here.
+	 */
+	void ApplyConfigSection(const ConfigSection &config)
+	{
+		config.Get("field_strength", FieldStrength);
+
+	}
+
+	/*
+	 * Called once every timestep
+	 */
+	void CurTimeUpdated()
+	{
+		;
+	}
+
+	/*
+	 * Called for every grid point at every time step. 
+	 */
+	inline double GetPotentialValue(const blitz::TinyVector<double, Rank> &pos)
+	{
+		double r = std::abs(pos(0));
+		double theta = pos(1);
+
+		double z = r * cos(theta);
+		return FieldStrength * z;
+	}
+};
+
+template<int Rank>
+class CoulombPotential : public PotentialBase<Rank>
+{
+public:
+	//Required by DynamicPotentialEvaluator
+	cplx TimeStep;
+	double CurTime;
+
+	//Potential parameters
+	double charge;
+
+	/*
+	 * Called once with the corresponding config section
+	 * from the configuration file. Do all one time set up routines
+	 * here.
+	 */
+	void ApplyConfigSection(const ConfigSection &config)
+	{
+		config.Get("charge", charge);
+	}
+
+	/*
+	 * Called for every grid point at every time step. 
+	 *
+	 * Some general tips for max efficiency:
+	 * - If possible, move static computations to ApplyConfigSection.
+	 * - Minimize the number of branches ("if"-statements are bad)
+	 * - Minimize the number of function calls (sin, cos, exp, are bad)
+	 * - Long statements can confuse the compiler, consider making more 
+	 *   simpler statements
+	 */
+	inline double GetPotentialValue(const blitz::TinyVector<double, Rank> &pos)
+	{
+		double r = std::abs(pos(0));
+		if (r < 10e-5)
+		{
+			return 0;
+		}
+		double V = charge / r;
+
+		return V;
+	}
+};
+
+template<int Rank>
+class CentrifugalPotential : public PotentialBase<Rank>
+{
+public:
+	//Required by DynamicPotentialEvaluator
+	cplx TimeStep;
+	double CurTime;
+
+	/*
+	 * Called once with the corresponding config section
+	 * from the configuration file. Do all one time set up routines
+	 * here.
+	 */
+	void ApplyConfigSection(const ConfigSection &config)
+	{
+		;
+	}
+
+	/*
+	 * Called for every grid point at every time step. 
+	 */
+	inline double GetPotentialValue(const blitz::TinyVector<double, Rank> &pos)
+	{
+		double r = std::fabs(pos(0));
+		if (r < 1e-5)
+		{
+			return 0.0;
+		}
+
+		double V = 1.0 / (r * r);
+
+		return V;
+	}
+};
