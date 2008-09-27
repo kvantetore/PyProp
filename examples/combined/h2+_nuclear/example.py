2058375ccf18937b325d4fff5aaa9d460b8a74b6
tore.birkeland <tore.birkeland@354cffb3-6528-0410-8d42-45ce437ad3c5>
2008-09-27 14:26:59 +0000

    First benchmark completed
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@467 354cffb3-6528-0410-8d42-45ce437ad3c5

--- examples/combined/h2+_nuclear/example.py
+++ examples/combined/h2+_nuclear/example.py
@@ -14,20 +14,43 @@ from libh2p import *
 from pylab import *
 from numpy import *
 
+from datetime import timedelta
+import commands
+
+#use a submitpbs wrapper depending on the installation
+#we are currently running from 
+installation = os.environ.get("INSTALLATION", "local")
+if installation == "hexagon":
+	import pyprop.utilities.submitpbs_hexagon as submitpbs
+if installation == "stallo":
+	import pyprop.utilities.submitpbs_stallo as submitpbs
+
+
 def SetupProblem(**args):
+	"""
+	Sets up a problem corresponding to the 
+	arguments given in **args
+	"""
 	conf = SetupConfig(**args)
 	prop = pyprop.Problem(conf)
 	prop.SetupStep()
 
 	return prop
 
+
 def FindEigenvalues(**args):
+	"""
+	Finds the eigenvalues of the system specified by **args,
+	by using pIRAM or ARPACK
+	"""
+
 	prop = SetupProblem(**args)
 	tempPsi = prop.GetTempPsi()
 
 	solver = pyprop.ArpackSolver(prop)
 	solver.Solve()
-	print solver.Solver.GetEigenvalues().real
+	
+	PrintOut( solver.Solver.GetEigenvalues().real )
 
 	#test if the find eigenvectors are really eigenvectors
 	for i in range(len(solver.Solver.GetEigenvalues())):
@@ -41,7 +64,7 @@ def FindEigenvalues(**args):
 
 		E = prop.GetEnergyExpectationValue()
 		
-		print "E = %f, cos(theta) = %f" % (E, abs(lindep))
+		PrintOut(  "E = %f, cos(theta) = %f" % (E, abs(lindep)) )
 
 		#Propagate the eigenstate to see if it remains in the same state
 		#args['silent'] = True
@@ -50,7 +73,40 @@ def FindEigenvalues(**args):
 
 	return solver
 
+
+def EstimateRuntime(prop, stepCount=50):
+	"""
+	Estimate runtime of propagating the Problem prop
+	to end, by running a few timesteps, and assuming that
+	every timestep will take the same time to execute
+	"""
+
+	#time at beginning of propagation
+	timestepDuration = - time.time()
+
+	#propagate
+	for i in range(stepCount):
+		prop.AdvanceStep()
+
+	#time at end of propagation
+	timestepDuration += time.time()
+
+	timestepDuration /= stepCount
+	estimatedRuntime = abs(prop.Duration / prop.TimeStep) * timestepDuration
+
+	#Print out estimated runtime
+	fmt = time.gmtime(estimatedRuntime)
+	PrintOut("Estimated Runtime = %i days, %i hours, %i minutes, timestep = %f" % (fmt[2]-1, fmt[3], fmt[4], timestepDuration))
+
+	return timestepDuration, estimatedRuntime
+
+
 def FindGroundstate(**args):
+	"""
+	Finds the lowest eigenvalue and the corresponding eigenstate 
+	of the system specified by args by propagating with imaginary time
+	"""
+	
 	silent = pyprop.ProcId != 0
 	args["silent"] = silent
 
@@ -61,21 +117,7 @@ def FindGroundstate(**args):
 	prop = SetupProblem(**args)
 	prop.AdvanceStep()
 
-	#Estimate runtime by running a few timesteps
-	stepCount = 50
-	timestepDuration = - time.time()
-	for i in range(stepCount):
-		prop.AdvanceStep()
-	timestepDuration += time.time()
-	timestepDuration /= stepCount
-
-	prop.psi.GetRepresentation().GetDistributedModel().GlobalBarrier()
-	
-	estimatedRuntime = abs(prop.Duration / prop.TimeStep) * timestepDuration
-	fmt = time.gmtime(estimatedRuntime)
-	PrintOut("Estimated Runtime = %i days, %i hours, %i minutes (%f secs), timestep = %f" % (fmt[2]-1, fmt[3], fmt[4], estimatedRuntime, timestepDuration))
-
-	return 
+	EstimateRuntime(prop)
 
 	#propagate
 	for t in prop.Advance(10):
@@ -113,18 +155,6 @@ def Propagate(**args):
 	initPsi.Normalize()
 	prop.psi.GetData()[:] = initPsi.GetData()
 
-	#Estimate runtime by running a few timesteps
-	stepCount = 10
-	timestepDuration = - time.time()
-	for i in range(stepCount):
-		prop.AdvanceStep()
-	timestepDuration += time.time()
-	timestepDuration /= stepCount
-
-	estimatedRuntime = abs(prop.Duration / prop.TimeStep) * timestepDuration
-	fmt = time.gmtime(estimatedRuntime)
-	PrintOut("Estimated Runtime = %i days, %i hours, %i minutes" % (fmt[2]-1, fmt[3], fmt[4]))
-
 	def output():
 		norm = prop.psi.GetNorm()
 		corr = abs(prop.psi.InnerProduct(initPsi))**2
@@ -134,6 +164,8 @@ def Propagate(**args):
 
 	#Output once before the propagation
 	output()
+
+	EstimateRuntime(prop)
 
 	#Propagated to end with 100 printouts
 	for t in prop.Advance(100):
@@ -185,7 +217,7 @@ def SetupConfig(**args):
 	will return a conf object loading h2p.ini, setting timestep to -0.04mj, and 
 	renormalization to True.
 	"""
-	silent = False
+	silent = pyprop.ProcId == 0
 	if 'silent' in args:
 		silent = args['silent']
 	
@@ -199,33 +231,33 @@ def SetupConfig(**args):
 	#Grid size
 	if 'electronicGridSize' in args:
 		gridSize = args['electronicGridSize']
-		if not silent: print "Using Electronic Grid Size = ", gridSize
+		PrintOut( "Using Electronic Grid Size = %i" % gridSize )
 		rank0 = conf.ElectronicRadialRepresentation.rank0
 		rank0[2] = gridSize
 		conf.SetValue("ElectronicRadialRepresentation", "rank0", rank0)
 
 	if 'nuclearGridSize' in args:
 		gridSize = args['nuclearGridSize']
-		if not silent: print "Using Nuclear Grid Size = ", gridSize
+		PrintOut( "Using Nuclear Grid Size = %s" % gridSize)
 		rank0 = conf.NuclearRadialRepresentation.rank0
 		rank0[2] = gridSize
 		conf.SetValue("NuclearRadialRepresentation", "rank0", rank0)
 
 	if 'lmax' in args:
 		lmax = args['lmax']
-		if not silent: print "Using LMax = ", lmax
+		PrintOut("Using LMax = %s" % lmax)
 		conf.SetValue("ElectronicAngularRepresentation", "maxl", lmax)
 
 	#TimeStep
 	if 'dt' in args:
 		dt = args['dt']
-		if not silent: print "Using TimeStep = ", dt
+		PrintOut("Using TimeStep = %s" % dt)
 		conf.SetValue("Propagation", "timestep", dt)
 
 	#Imaginary Time Propagation
 	if 'imTime' in args:
 		imTime = args['imTime']
-		if not silent: print "Using ImaginaryTime = ", imTime
+		PrintOut("Using ImaginaryTime = %s" % imTime)
 		dt = conf.Propagation.timestep
 		if imTime:
 			conf.SetValue("Propagation", "renormalization", True)
@@ -243,10 +275,10 @@ def SetupConfig(**args):
 		silent = args['silent']
 		conf.SetValue("Propagation", "silent", silent)
 
-		#H2+ Potential
+	#H2+ Potential
 	if 'softing' in args:
 		softing = args['softing']
-		if not silent: print "Using softing ", softing
+		PrintOut("Using softing %s" % softing)
 		conf.SetValue("Potential", "softing", softing)
 
 	#Laser Potential
@@ -254,9 +286,50 @@ def SetupConfig(**args):
 		duration = args['duration']
 		conf.SetValue("Propagation", "duration", duration)
 
+	#Parallelization
+	if "procArrayRank" in args:
+		procArrayRank = args["procArrayRank"]
+		conf.SetValue("Distribution", "proc_array_rank", procArrayRank)
+
+	if "initDistr" in args:
+		initDistr = array(args["initDistr"])
+		conf.SetValue("Distribution", "initial_distribution", initDistr)
 	
-	print "Setup Config Complete"
-	print ""
+	PrintOut( "Setup Config Complete" )
+	
 	return conf
+
+
+
+def Submit(executable=None, runHours=3, runMinutes=0, jobname="pyprop", printScript=False, **args):
+	"""
+	Set up a job script suitable 
+	"""
+
+	#Create jobscript 
+	jscript = submitpbs.SubmitScript()
+	jscript.jobname = jobname
+	jscript.walltime = timedelta(hours=runHours, minutes=runMinutes, seconds=0)
+	jscript.ppn = args.get("ppn", 4)
+	jscript.proc_memory = args.get("proc_memory", "1000mb")
+	jscript.nodes = args.get("nodes", "1")
+	numProcs = args.get("numProcs", jscript.ppn*jscript.nodes)
+	if installation == "stallo":
+		jscript.interconnect = args.get("interconnect", "ib")
+		jscript.workingdir = args.get("workingDir", os.path.abspath(os.curdir) )
+		jscript.executable = "mpirun -n %s " % (jscript.ppn*jscript.nodes)
+		jscript.executable += "python %s" % executable
+	elif installation == "hexagon":
+		jscript.workingdir = args.get("workingDir", os.path.abspath(os.curdir) )
+		jscript.executable = "aprun -n %s -N %s " % (numProcs, jscript.ppn)
+		jscript.executable += "./python-exec %s" % executable
+	jscript.parameters = commands.mkarg(repr(args))
+
+	#Submit this job
+	if printScript:
+		print "\n".join(jscript.CreateScript())
+	else:
+		jscript.Submit()
+
 
 
