2742a941da5565377668ea5bd7df37e85fc43cc6
raymond.nepstad <raymond.nepstad@354cffb3-6528-0410-8d42-45ce437ad3c5>
2008-12-21 20:00:12 +0000

    updated tensor two-electron test functions
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@527 354cffb3-6528-0410-8d42-45ce437ad3c5

--- examples/tensor/twoelectron_test.py
+++ examples/tensor/twoelectron_test.py
@@ -1,29 +1,33 @@
 
-def SetupBigMatrix(prop, whichPotential):
+def SetupBigMatrix(prop, whichPotentials):
 	print "Setting up potential matrix..."
 	matrixSize = prop.psi.GetData().size
-	potential = prop.Propagator.BasePropagator.PotentialList[whichPotential]
-
+	
+	#Allocate the hamilton matrix
+	print "    Allocating potential matrix of size [%i, %i]  ~%.0f MB" % (matrixSize, matrixSize, matrixSize**2 * 16 / 1024.**2)
 	BigMatrix = zeros((matrixSize, matrixSize), dtype="complex")
 
-	basisPairs1 = potential.BasisPairs[0]
-	basisPairs2 = potential.BasisPairs[1]
-	basisPairs3 = potential.BasisPairs[2]
-	
-	GetCoupledIndex = prop.psi.GetRepresentation().GetRepresentation(2).Range.GetCoupledIndex
-	r1Count = prop.psi.GetData().shape[0]
-	r2Count = prop.psi.GetData().shape[1]
-	lCount = prop.psi.GetData().shape[2]
+	for potNum in whichPotentials:
+		potential = prop.Propagator.BasePropagator.PotentialList[potNum]
+		print "    Processing potential %i: %s" % (potNum, potential.Name)
 
-	for i, (A, Ap) in enumerate(basisPairs3):
-		for j, (r1,r1p) in enumerate(basisPairs1):
-			for k, (r2,r2p) in enumerate(basisPairs2):
-				#indexLeft = (A*r1Count*r2Count) + (r1 * r2Count) + r2
-				#indexRight = (Ap*r1Count*r2Count) + (r1p * r2Count) + r2p
-				indexLeft = (r1 * r2Count*lCount) + (r2*lCount) + A
-				indexRight = (r1p * r2Count*lCount) + (r2p*lCount) + Ap
-				BigMatrix[indexLeft, indexRight] = potential.PotentialData[j, k, i]
+		basisPairs1 = potential.BasisPairs[0]
+		basisPairs2 = potential.BasisPairs[1]
+		basisPairs3 = potential.BasisPairs[2]
+		
+		#GetCoupledIndex = prop.psi.GetRepresentation().GetRepresentation(2).Range.GetCoupledIndex
+		r1Count = prop.psi.GetData().shape[0]
+		r2Count = prop.psi.GetData().shape[1]
+		lCount = prop.psi.GetData().shape[2]
 
+		for i, (A, Ap) in enumerate(basisPairs3):
+			for j, (r1,r1p) in enumerate(basisPairs1):
+				for k, (r2,r2p) in enumerate(basisPairs2):
+					#indexLeft = (A*r1Count*r2Count) + (r1 * r2Count) + r2
+					#indexRight = (Ap*r1Count*r2Count) + (r1p * r2Count) + r2p
+					indexLeft = (r1 * r2Count*lCount) + (r2*lCount) + A
+					indexRight = (r1p * r2Count*lCount) + (r2p*lCount) + Ap
+					BigMatrix[indexLeft, indexRight] += potential.PotentialData[j, k, i]
 
 	return BigMatrix
 
