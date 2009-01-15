a5785878f2cee8e3d5b94c7fb030123d11b80885
raymond.nepstad <raymond.nepstad@354cffb3-6528-0410-8d42-45ce437ad3c5>
2009-01-15 20:51:22 +0000

    implemented __repr__ on DefaultCoupledIndexIterator
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@578 354cffb3-6528-0410-8d42-45ce437ad3c5

--- pyprop/CoupledSphericalHarmonics.py
+++ pyprop/CoupledSphericalHarmonics.py
@@ -3,7 +3,7 @@
 from core import CoupledIndex
 CoupledIndex.__str__ = lambda self: "l1=%i, l2=%i, L=%i, M=%i" % (self.l1, self.l2, self.L, self.M)
 
-def DefaultCoupledIndexIterator(lmax, L=None, M=[0], fullTensor=False):
+class DefaultCoupledIndexIterator:
 	"""
 	Creates an iterator for giving all possible values of l1, l2, L, M to
 	the CoupledSphericalHarmonicRepresentation
@@ -30,26 +30,34 @@ def DefaultCoupledIndexIterator(lmax, L=None, M=[0], fullTensor=False):
 	index_iterator = DefaultCoupledIndexIterator(lmax=4, L=[0,1,2], M=0)
 
 	"""
-	if L==None:
-		L = range(lmax+1)
+	def __init__(self, lmax, L=None, M=[0], fullTensor=False):
 
-	#Make sure L and M are iterable
-	if not hasattr(L, "__iter__"):
-		L = [L]
-	if not hasattr(L, "__iter__"):
-		M = [M]
+		if L==None:
+			L = range(lmax+1)
 
-	#Iterate through all permutations
-	for curM in M:
-		for curL in L:
-			for l1 in range(lmax+1):
-				for l2 in range(lmax+1):
-					#Only yield if l1, l2, L satisfies clebsch gordan condition
-					#or we have explicitly asked fo the full tensor product of states
-					if (abs(l1 - l2) <= curL <= l1 + l2) or fullTensor:
-						yield CoupledIndex(l1, l2, curL, curM)
+		#Make sure L and M are iterable
+		if not hasattr(L, "__iter__"):
+			L = [L]
+		if not hasattr(L, "__iter__"):
+			M = [M]
 
+		self.L = L
+		self.M = M
+		self.lmax = lmax
+		self.FullTensor = fullTensor
 
-
-
+	def __iter__(self):
+		#Iterate through all permutations
+		for curM in self.M:
+			for curL in self.L:
+				for l1 in range(self.lmax+1):
+					for l2 in range(self.lmax+1):
+						#Only yield if l1, l2, L satisfies clebsch gordan condition
+						#or we have explicitly asked fo the full tensor product of states
+						if (abs(l1 - l2) <= curL <= l1 + l2) or self.FullTensor:
+							yield CoupledIndex(l1, l2, curL, curM)
 		
+
+	def __repr__(self):
+		return "sys.modules['pyprop'].DefaultCoupledIndexIterator(%s, L=%s, M=%s, fullTensor=%s)" % \
+			(self.lmax, self.L, self.M, self.FullTensor)
