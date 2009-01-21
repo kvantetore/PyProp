629346627df0a693042fdc1e9f8feb888b51107b
raymond.nepstad <raymond.nepstad@354cffb3-6528-0410-8d42-45ce437ad3c5>
2009-01-21 13:33:29 +0000

    cleanup in propagator folder (removed oct lib)
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@596 354cffb3-6528-0410-8d42-45ce437ad3c5

--- pyprop/propagator/init.py
+++ pyprop/propagator/init.py
@@ -14,16 +14,6 @@ execfile(__path__[0] + "/propagator/OdePropagator.py")
 execfile(__path__[0] + "/propagator/RungeKuttaPropagator.py")
 execfile(__path__[0] + "/propagator/VectorPropagator.py")
 execfile(__path__[0] + "/propagator/CayleyPropagator.py")
-#execfile(__path__[0] + "/propagator/Krotov.py")
 
 #init subpropagators
 execfile(__path__[0] + "/propagator/subpropagator/init.py")
-
-#init optimal control solvers
-#try:
-#execfile(__path__[0] + "/propagator/optimalcontrol/init.py")
-oct_module_path = os.environ["OCT_PYPROP_MODULE_PATH"]
-execfile(oct_module_path + "/init.py")
-execfile(__path__[0] + "/propagator/Krotov.py")
-#except:
-#	print "Oops, could not load optimal control solvers!"
