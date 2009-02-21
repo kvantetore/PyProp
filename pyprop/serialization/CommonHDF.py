d1a477ab652f2d5ee8882c9c719c68938783540c
tore.birkeland <tore.birkeland@354cffb3-6528-0410-8d42-45ce437ad3c5>
2009-02-21 16:47:01 +0000

    oops
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@669 354cffb3-6528-0410-8d42-45ce437ad3c5

---
+++ pyprop/serialization/CommonHDF.py
@@ -0,0 +1,224 @@
+import numpy 
+import os
+import time
+
+from numpy import r_, s_, all, diff, array, asarray
+
+DEBUG = False
+
+#--------------------------------------------------------------------------------------
+#                         Dataset Tools
+#--------------------------------------------------------------------------------------
+
+def RemoveExistingDataset(filename, datasetPath):
+	"""
+	Removes a node from a HDF5 file if it exists
+	otherwise, do nothing
+	"""
+	if not os.path.exists(filename):
+		return
+
+	f = tables.openFile(filename, "a")
+	try:
+		groupName, datasetName = GetDatasetName(datasetPath)
+		try:
+			f.removeNode(groupName, datasetName)
+		except tables.NoSuchNodeError:
+			pass
+			
+	finally:
+		f.close()
+
+
+def GetDatasetName(datasetPath):
+	"""
+	Translates a dataset path into a groupName and a datasetName returned
+	as a tuple
+	"""
+	#get dataset
+	if not datasetPath.startswith("/"):
+		datasetPath = "/"+ datasetPath
+	groupName = os.path.dirname(datasetPath)
+	datasetName = os.path.basename(datasetPath)
+	return groupName, datasetName
+
+
+def GetExistingDataset(f, datasetPath):
+	"""
+	Get an existing dataset at the given path. None is returned if it does 
+	not exist.
+	"""
+	if not datasetPath.startswith("/"):
+		datasetPath = "/"+ datasetPath
+
+	#check if dataset already exists
+	dataset = None
+	try:
+		dataset = f.getNode(datasetPath)
+	except tables.NoSuchNodeError:
+		pass
+
+	return dataset
+
+
+def CreateDataset(f, datasetPath, fullShape):
+	"""
+	Creates a chunked array dataset of shape fullShape at the given path. 
+	"""
+	#Separate path from node name 
+	groupName, datasetName = GetDatasetName(datasetPath)
+
+	#Iterative over path (for each /), determine if each node
+	#in the path exists, if not, create it
+	pathList = groupName.split("/")
+	curPath = ""
+	prevPath = ""
+	for curGroupName in pathList:
+		#Skip zero-length node names (from the split operation)
+		if len(curGroupName) == 0:
+			continue
+
+		#Create node if it does not exist
+		prevPath = curPath
+		curPath += "/%s" % curGroupName
+		if not f.__contains__(curPath):
+			if prevPath == "":
+				newGroup = f.createGroup("/", curGroupName)
+			else:
+				newGroup = f.createGroup(prevPath, curGroupName)
+
+	#Finally, save the data set
+	atom = tables.ComplexAtom(itemsize=16)
+	filters = tables.Filters(complevel=0)
+	dataset = f.createCArray(groupName, datasetName, atom, fullShape, filters=filters)
+
+	return dataset
+
+#--------------------------------------------------------------------------------------
+#                         Load and Save slabs of large datasets
+#--------------------------------------------------------------------------------------
+
+def SaveLocalSlab(filename, datasetPath, localData, localSlab, fullShape):
+	"""
+	Saves the local slab of a global dataset to a file
+
+	filename    - the hdf5-file to save to
+	datasetPath - the path in <filename> where the dataset should be stored
+	localData   - the n-dimensional array containing the local data to be stored
+	localSlab   - the local slab (n-dimensional tuple containing the local range in
+				  the global dataset
+	fullShape   - shape of the global dataset
+
+	"""
+	#open file
+	f = tables.openFile(filename, "a")
+	try:
+		#get dataset
+		localShape = localData.shape
+		dataset = GetExistingDataset(f, datasetPath)
+		if dataset == None:
+			#create new dataset
+			dataset = CreateDataset(f, datasetPath, fullShape)
+		else:
+			#check that is has the correct size
+			if not dataset.shape == fullShape:
+				raise "Invalid shape on existing dataset. Got %s, expected %s" % (dataset.shape, fullShape)
+		
+		#write data
+		isSlices = map(lambda s: isinstance(s, slice), localSlab)
+		if all(isSlices):
+			dataset[localSlab] = localData
+		elif all(isSlices[1:]):
+			for dataIdx, fileIdx in enumerate(localSlab[0]):
+				curLocalSlab = (fileIdx,) + localSlab[1:]
+				dataset[curLocalSlab] = localData[dataIdx, :]
+		else:
+			raise "Only the first rank may be fancy indexing"
+
+	
+	finally:
+		#Make sure file is closed
+		f.close()
+
+
+def LoadLocalSlab(filename, datasetPath, localData, localSlab, fullShape):
+	"""
+	Loads the local slab of a global dataset from a file
+
+	filename    - the hdf5-file to load from
+	datasetPath - the path in <filename> where the dataset is stored
+	localData   - the n-dimensional array where to store the local data
+	localSlab   - the local slab (n-dimensional tuple containing the local range in
+				  the global dataset
+	fullShape   - shape of the global dataset
+
+	"""
+	#open file
+	f = tables.openFile(filename, "a")
+	try:
+		#get dataset
+		localShape = localData.shape
+		dataset = GetExistingDataset(f, datasetPath)
+		if dataset == None:
+			raise Exception("Dataset '%s' not found in file '%s'" % (datasetPath, filename))
+		else:
+			#check that is has the correct size
+			if not dataset.shape == fullShape:
+				raise Exception("Invalid shape on existing dataset. Got %s, expected %s" % (dataset.shape, fullShape))
+		
+		#load data
+		isSlices = map(lambda s: isinstance(s, slice), localSlab)
+		if all(isSlices):
+			localData[:] = dataset[localSlab]
+		elif all(isSlices[1:]):
+			for dataIdx, fileIdx in enumerate(localSlab[0]):
+				curLocalSlab = (fileIdx,) + localSlab[1:]
+				localData[dataIdx, :] = dataset[curLocalSlab]
+		else:
+			raise "Only the first rank may be fancy indexing"
+
+	finally:
+		#Make sure file is closed
+		f.close()
+
+
+#--------------------------------------------------------------------------------------
+#                         Config Serialization
+#--------------------------------------------------------------------------------------
+
+def SaveConfigObject(filename, datasetPath, conf):
+	if conf != None:
+		h5file = tables.openFile(filename, "r+")
+		try:
+			dataset = GetExistingDataset(h5file, datasetPath)
+			if datasetPath:
+				h5file.setNodeAttr(dataset, "configObject", conf.cfgObj)
+		except:
+			print "WARNING: Could not store config object!"
+		finally:
+			h5file.close()
+
+
+def GetConfigFromHDF5(file, datasetPath = None, confObjName = "configObject"):
+	"""
+	Load a configparser object stored as an attribute on a wavefunction in a 
+	HDF5 file. This can be used to recreate a pyprop.Config object, or the
+	original config file.
+	"""
+	h5file = tables.openFile(file, "r")
+	cfgObj = None
+	try:
+		if datasetPath == None:
+			for node in h5file.walkNodes():
+				#name = node._v_name
+				#if name == "wavefunction":
+				if confObjName in node._v_attrs._v_attrnames:
+					cfgObj = node._f_getAttr(confObjName)
+		else:
+			cfgObj = h5file.getNodeAttr(datasetPath, confObjName)
+
+	finally:
+		h5file.close()
+	
+	return cfgObj
+
---
+++ pyprop/serialization/TensorPotentialHDF.py
@@ -0,0 +1,157 @@
+"""
+(De)Serialization routines for Tensor Potentials
+
+use the methods SaveTensorPotential and LoadTensorPotential
+to load and save tensor potentials instead of regenerating
+them every time
+"""
+
+
+def SaveTensorPotential(filename, datasetPath, potential, distributedModel, conf=None):
+	"""
+	Saves a tensor potential to a dataset in a HDF file, and stores a config object
+	as an attribute.
+
+	Parameters
+	--------------------------------------------------------------------
+	- filename is a string containing a filename to a hdffile which 
+	may or may not exist. 
+	
+	- datasetPath is the path to the dataset where the wavefunction data
+	should be stored.
+
+	- potential is the TensorPotential object to be saved
+
+	- distributedModel is a distributed model from a wavefunction decribing 
+	the current distribution of things
+	
+	"""
+
+	distr = distributedModel
+	localData = potential.PotentialData
+	localShape = localData.shape
+	localSlab = tuple(map(GetLocalBasisPairSlice, potential.GeometryList))
+	fullShape = tuple(distr.GetGlobalShape(array(localShape)))
+
+	if distr.IsSingleProc():
+		t = - time.time()
+		RemoveExistingDataset(filename, datasetPath)
+		SaveLocalSlab(filename, datasetPath, localData, localSlab, fullShape)
+		t += time.time()
+		if DEBUG: print "Duration: %.10fs" % t
+		SaveConfigObject(filename, datasetPath, conf)
+		SaveGeometryInfo(filename, datasetPath, potential.GeometryList)
+
+	else:
+		#let the processors save their part one by one
+		procCount = distr.ProcCount
+		procId = distr.ProcId
+		localSize = localData.nbytes / 1024.**2
+		distr.GlobalBarrier()
+		if procId==0 and DEBUG: print "Saving %s..." % filename
+		for i in range(procCount):
+			if procId == i:
+				if procId == 0:
+					RemoveExistingDataset(filename, datasetPath)
+
+				if DEBUG: print "    Process %i writing hyperslab of %iMB" % (procId, localSize)
+				t = - time.time()
+				SaveLocalSlab(filename, datasetPath, localData, localSlab, fullShape)
+				t += time.time()
+				if DEBUG: print "    Duration: %.10fs" % t
+
+			distr.GlobalBarrier();
+		if procId==0 and DEBUG: print "Done."
+
+		#proc 0 save attribs
+		if procId == 0:
+			#config object
+			SaveConfigObject(filename, datasetPath, conf)
+			#basis pairs
+			SaveGeometryInfo(filename, datasetPath, potential.GeometryList)
+
+
+		#Make sure everyone is finished
+		distr.GlobalBarrier();
+
+
+def LoadTensorPotential(filename, datasetPath, potential, distributedModel):
+	"""
+	Loads the tensor potential data from a dataset in a HDF file
+	"""
+
+	distr = distributedModel
+	localData = potential.PotentialData
+	localShape = localData.shape
+	localSlab = tuple(map(GetLocalBasisPairSlice, potential.GeometryList))
+	fullShape = tuple(distr.GetGlobalShape(array(localShape)))
+
+	if not CheckLocalSlab(filename, datasetPath, potential.GeometryList, localSlab):
+		raise Exception("Organization of basis pairs has changed from when this potential was generated. Please regenerate potential")
+
+	if distr.IsSingleProc():
+		t = - time.time()
+		LoadLocalSlab(filename, datasetPath, localData, localSlab, fullShape)
+		t += time.time()
+		if DEBUG: print "Duration: %.10fs" % t
+
+	else:
+		#let the processors save their part one by one
+		procCount = distr.ProcCount
+		procId = distr.ProcId
+		localSize = localData.nbytes / 1024.**2
+		distr.GlobalBarrier()
+		if procId==0 and DEBUG: print "Saving %s..." % filename
+		for i in range(procCount):
+			if procId == i:
+				if DEBUG: print "    Process %i writing hyperslab of %iMB" % (procId, localSize)
+				t = - time.time()
+				LoadLocalSlab(filename, datasetPath, localData, localSlab, fullShape)
+				t += time.time()
+				if DEBUG: print "    Duration: %.10fs" % t
+
+			distr.GlobalBarrier();
+		if procId==0 and DEBUG: print "Done."
+
+		#Make sure everyone is finished
+		distr.GlobalBarrier();
+
+
+def GetLocalBasisPairSlice(geomInfo):
+	"""
+	Gets the local slab of a 
+	"""
+	basisPairIndices = asarray(geomInfo.GetLocalBasisPairIndices(), dtype=int)
+	if (diff(basisPairIndices) == 1).all():
+		basisPairIndices = s_[basisPairIndices[0]:basisPairIndices[0]+len(basisPairIndices)]
+	return basisPairIndices
+
+
+def CheckLocalSlab(filename, datasetPath, geometryList, localSlab):
+	rank = len(localSlab)
+	f = tables.openFile(filename, "a")
+	try:
+		node = GetExistingDataset(f, datasetPath)
+		globalBasisPairList = map(lambda i: getattr(node._v_attrs, "basisPairs%i" % i), r_[:rank])
+	finally:
+		f.close()
+
+	for origGlobalBasisPairs, localIndices, geomInfo in zip(globalBasisPairList, localSlab, geometryList):
+		newBasisPairs = geomInfo.GetGlobalBasisPairs()[localIndices]
+		origBasisPairs = origGlobalBasisPairs[localIndices]
+		if not (origBasisPairs == newBasisPairs).all():
+			return False
+	return True
+
+	
+def SaveGeometryInfo(filename, datasetPath, geometryList):
+	f = tables.openFile(filename, "a")
+	try:
+		node = GetExistingDataset(f, datasetPath)
+		for i, geom in enumerate(geometryList):
+			setattr(node._v_attrs, "basisPairs%i" % i, geom.GetGlobalBasisPairs())
+	finally:
+		f.close()
+
+
+
