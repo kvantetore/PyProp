803fdec7db6ce7e5cb481a44aa61b438af8998c9
raymond.nepstad <raymond.nepstad@354cffb3-6528-0410-8d42-45ce437ad3c5>
2009-06-05 12:49:55 +0000

    now possible to set up exponential-linear sequence (bsplines) from a different set of parameters
    git-svn-id: https://pyprop.googlecode.com/svn/trunk@745 354cffb3-6528-0410-8d42-45ce437ad3c5

--- pyprop/bspline/breakpointsequences.py
+++ pyprop/bspline/breakpointsequences.py
@@ -1,4 +1,7 @@
 from numpy import *
+from scipy.optimize import fsolve
+
+DEBUG = False
 		
 
 def ExponentialBreakpointSequence(rmin, rmax, n, gamma):
@@ -87,3 +90,95 @@ def ExponentialLinearBreakpointSequence(rmin, rpartition, rmax, n, gamma):
 
 	return xi
 
+
+def VarExponentialLinearBreakpointSequence(rmin, rmax, minSep, maxSep, N):
+	"""
+	Same as VarExponentialLinearBreakpointSequence2, but solves a different set of
+	equations.
+	"""
+	
+	def pointsInner(h):
+		return int(round(N - (rmax - rmin) / maxSep - h / maxSep - 1))
+
+	def Equation1(gam, h, n):
+		#n = pointsInner(h)
+		return h * (exp(gam/(n-1.0)) - 1.0) / (exp(gam) - 1.0) - minSep
+
+	def Equation2(gam, h, n):
+		#n = pointsInner(h)
+		return h * (exp(gam) - exp(gam * (n - 2.0)/(n - 1.0))) / (exp(gam) - 1.0) - maxSep
+
+	def Equation3(gam, h, n):
+		return N - (rmax - rmin) / maxSep - h / maxSep - 1 - n
+
+	def func(x):
+		out = [Equation1(x[0], x[1], x[2])]
+		out.append(Equation2(x[0], x[1], x[2]))
+		out.append(Equation3(x[0], x[1], x[2]))
+
+		return out
+
+	#Solve system of nonlinear equations to obtain gamma and h
+	gam, h, n = fsolve(func, [2.0, 10.0, 20])
+
+	n = ceil(n)
+
+	#Calculate partition point
+	rpartition = rmin + h
+
+	if DEBUG:
+		print "gamma = %s" % gam
+		print "rpartition = %s" % rpartition
+		print "number of inner points = %s" % n
+		print "Equation 1 = %s" % Equation1(gam, h, n)
+		print "Equation 2 = %s" % Equation2(gam, h, n)
+
+	return ExponentialLinearBreakpointSequence(rmin, rpartition, rmax, n, gam)
+
+
+def VarExponentialLinearBreakpointSequence2(rmin, rmax, minSep, maxSep, N):
+	"""
+	Compute exponential-linear sequence from a different set of parameters: grid extension,
+	smallest and largest point seperatation, and total number of points. A non-linear system
+	of equations is solved to find the parameters that best fulfill these conditions (gamma,
+	number of inner points and extension of the exponential part). 
+	"""
+	
+	def pointsInner(h):
+		return int(round(N - (rmax - rmin) / maxSep - h / maxSep - 1))
+
+	def Equation1(gam, rpartition, n):
+		n = round(n)
+		return abs(len(ExponentialLinearBreakpointSequence(rmin, rpartition, rmax, n, gam)) - N)
+
+	def Equation2(gam, rpartition, n):
+		n = round(n)
+		xi = ExponentialLinearBreakpointSequence(rmin, rpartition, rmax, n, gam)
+		return abs(xi[1] - xi[0] - minSep)
+
+	def Equation3(gam, rpartition, n):
+		n = round(n)
+		xi = ExponentialLinearBreakpointSequence(rmin, rpartition, rmax, n, gam)
+		return abs(xi[-2] - xi[-1] - maxSep)
+
+	def func(x):
+		out = [Equation1(x[0], x[1], x[2])]
+		out.append(Equation2(x[0], x[1], x[2]))
+		out.append(Equation3(x[0], x[1], x[2]))
+
+		return out
+
+	#Solve system of nonlinear equations to obtain gamma and h
+	gam, rpartition, n = fsolve(func, [3.0, 10.0, 20])
+
+	n = ceil(n)
+
+	if DEBUG:
+		print "gamma = %s" % gam
+		print "rpartition = %s" % rpartition
+		print "number of inner points = %s" % n
+		print "Equation 1 = %s" % Equation1(gam, rpartition, n)
+		print "Equation 2 = %s" % Equation2(gam, rpartition, n)
+		print "Equation 3 = %s" % Equation2(gam, rpartition, n)
+
+	return ExponentialLinearBreakpointSequence(rmin, rpartition, rmax, n, gam)
