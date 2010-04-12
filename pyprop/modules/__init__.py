2cc66256d03f9de9f15fcf5651b5a90308c6f553
Tore Birkeland <tore.birkeland@gmail.com>
2010-04-12 10:46:04 +0200

    added docstring to modules

---
+++ pyprop/modules/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+Super-package for all pyprop modules
+
+pyprop modules are organized into three categories
+  discretizations - representations and the corresponding transforms and propagators.
+  solvers - solvers and propagators which are independent of representation
+  potentials - implementation of common operators/potentials
+  
+the utility module redirect for redirecting stdout is also placed at this level  
+"""
