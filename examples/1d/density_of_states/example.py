6262ee47586f5d30ea267be8327b595625329ef9
Raymond Nepstad <raymond.nepstad@gmail.com>
2011-03-31 14:20:17 +0200

    Improve density of state example

--- examples/1d/density_of_states/example.py
+++ examples/1d/density_of_states/example.py
@@ -1,7 +1,11 @@
-from numpy.linalg import eigh
-from numpy import sqrt, pi
+import scipy, scipy.linalg
+from numpy import sqrt, pi, zeros, double, array, abs, sum, argsort, real
+from numpy import conj, dot, diff
 import pyprop
 
+def AddToProjectNamespace(obj):
+	pyprop.ProjectNamespace[obj.__name__] = obj
+	return obj
 
 def CalculateDensityOfStates(configFile):
 
@@ -16,11 +20,16 @@ def CalculateDensityOfStates(configFile):
 	E, V = SetupEigenstates(prop)
 
 	#Calculate DOS
-	dos = 1.0/diff(E[0])
+	dos = 1.0/diff(E)
 
 	#Calculate exact dos
 	xmax = conf.RadialRepresentation.xmax
-	dos_exact = xmax / ( pi *sqrt(2 * E[0][1:]) )
+	dos_exact = xmax / ( pi *sqrt(2 * E[1:]) )
+
+	#Estimate highest reliable energy, about 2/3*E_max for B-splines
+	maxIdx = int(2*len(E) / 3.0)
+	maxReliableEnergy = E[maxIdx]
+	print "Estimated highest reliable energy = %1.1e" % maxReliableEnergy
 
 	return E[1:], dos_exact, dos
 
@@ -35,41 +44,28 @@ def SetupEigenstates(prop, potentialIndices=[0]):
 	and using the generalized eigenvalue solver in scipy to find all
 	eigenvalues and vectors. 
 	
-	eigenvalues is a list of 1-d eigenvalue arrays. Each array corresponding
-	to a
+	eigenvalues is a list of 1-d eigenvalue arrays. 	
+	
 	"""
+
 	if not pyprop.IsSingleProc():
 		raise Exception("Works only on a single processor")
 
 	S = SetupOverlapMatrix(prop)
-	bspl = prop.psi.GetRepresentation().GetRepresentation(1).GetBSplineObject()
-	phaseGrid = array((0, bspl.GetBreakpointSequence()[1]), dtype=double)
-	phaseBuffer = zeros(2, dtype=double)
 
-	eigenValues = []
-	eigenVectors = []
-
-	lCount = prop.psi.GetData().shape[0]
-	eigenvectorScaling = 1
-
-	M = SetupRadialMatrix(prop, potentialIndices, l)
+	pot = prop.Propagator.BasePropagator.PotentialList[0]
+	M = SetupHamiltonMatrix(prop.psi, pot)
 
 	E, V = scipy.linalg.eigh(a=M, b=S)
 
 	idx = argsort(real(E))
 	E = real(E[idx])
-	eigenValues.append(E)
+	eigenValues = E
 
 	#Sort and normalize eigenvectors
 	sNorm = lambda v: sqrt(abs(sum(conj(v) * dot(S, v))))
 	V = array([v/sNorm(v) for v in [V[:,idx[i]] for i in range(V.shape[1])]]).transpose()
-	eigenVectors.append(V)
-
-	#assure correct phase convention (first oscillation should start out real positive)
-	#for i, curE in enumerate(E):
-	#	bspl.ConstructFunctionFromBSplineExpansion(V[:,i].copy(), phaseGrid, phaseBuffer)
-	#	phase = arctan2(imag(phaseBuffer[1]), real(phaseBuffer[1]))
-	#	V[:,i] *= exp(-1.0j * phase)
+	eigenVectors = V
 
 	return eigenValues, eigenVectors
 
@@ -77,32 +73,65 @@ def SetupEigenstates(prop, potentialIndices=[0]):
 def SetupOverlapMatrix(prop):
 	overlap = prop.Propagator.BasePropagator.GeneratePotential(prop.Config.OverlapMatrixPotential)
 	overlap.SetupStep(0.)
-	matrix = SetupRadialMatrix(prop, [overlap])
+	matrix = SetupHamiltonMatrix(prop.psi, overlap)
 	return matrix
 
 
-def SetupHamiltonMatrix(prop, whichPotentials):
-	matrixSize = prop.psi.GetData().shape[0]
+def SetupHamiltonMatrix(psi, potential):
+	matrixSize = psi.GetData().shape[0]
 	matrix = zeros((matrixSize, matrixSize), dtype=double)
 
-	for potNum in whichPotentials:	
-		if isinstance(potNum, pyprop.TensorPotential):
-			potential = potNum
-		else:
-			potential = prop.Propagator.BasePropagator.PotentialList[potNum]
-		print "    Processing potential: %s" % (potential.Name, )
+	print "Processing potential: %s" % (potential.Name, )
 
-		angularBasisPairs = potential.BasisPairs[0]
-		idx = [idx for idx, (i,j) in enumerate(zip(angularBasisPairs[:,0], angularBasisPairs[:,1])) if i==j==angularIndex]
-		if len(idx) != 1:
-			raise "Invalid angular indices %s" % idx
-		idx = idx[0]
-
-		basisPairs = potential.BasisPairs[1]
-
-		for i, (x,xp) in enumerate(basisPairs):
-			indexLeft = x
-			indexRight = xp
-			matrix[indexLeft, indexRight] += potential.PotentialData[idx, i]
+	basisPairs = potential.BasisPairs[0]
+	for i, (x,xp) in enumerate(basisPairs):
+		indexLeft = x
+		indexRight = xp
+		matrix[indexLeft, indexRight] += potential.PotentialData[i]
 
 	return matrix
+
+
+def GetRadialGridInfo(conf):
+	"""Display radial grid information
+
+	From a config file, some information of the radial grid
+	is produced:
+
+	  -minimum radial spacing
+	  -maximum radial spacing
+	  -number of radial points
+
+	Input: pyprop config object
+	Returns: None
+
+	"""
+	distr = pyprop.CreateDistribution(conf)
+	repr = pyprop.CreateRepresentation(conf, distr)
+	bsplObj = repr.GetRepresentation(0).GetBSplineObject()
+	rGrid = bsplObj.GetBreakpointSequence()
+
+	print "Min. r-spacing: %f" % diff(rGrid)[0]
+	print "Max. r-spacing: %f" % diff(rGrid)[-1]
+	print "Num. of r points: %i" % len(rGrid)
+
+
+
+#---------------------------------------------------------------------------------------------------
+# Potentials
+#---------------------------------------------------------------------------------------------------
+@AddToProjectNamespace
+class KineticEnergyPotential:
+	def __init__(self):
+		self.TimeStep = None
+		self.CurTime = None
+
+	def ApplyConfigSection(self, conf):
+		self.Mass = conf.Get("mass")
+
+#	def GetPotentialValue(self, pos):
+#		return -1.0 / (2.0 * self.Mass)
+
+	def UpdatePotentialData(self, data, psi, dt, t):
+		data[:] = -1.0 / (2.0 * self.Mass)
+
